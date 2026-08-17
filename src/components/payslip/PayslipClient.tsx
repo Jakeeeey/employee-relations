8fe4cdc58d2dabe3691b3a9eaf0e299259fcc8ac
Erman <66209038+Xerman09@users.noreply.github.com>
2026-08-17 18:30:36 +0800

    fix changes in global file

--- src/components/payslip/PayslipClient.tsx
+++ src/components/payslip/PayslipClient.tsx
@@ -5,7 +5,7 @@ import { LockKeyhole, FileText, Download } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { PasswordPromptDialog } from "@/components/payslip/PasswordPromptDialog";
-import { buildPayslipsPdf } from "@/utils/payslipPdf";
+import { buildPayslipsPdf } from "@/modules/er/payslip/utils/payslipPdf";
 import { toast } from "sonner";
 
 export function PayslipClient() {
